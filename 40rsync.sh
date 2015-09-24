e6f35aa502937d577a87a283d4bf6c643d752640
Steve Sixty-Four <steve8x8@googlemail.com>
2015-09-24 20:41:27 +0200

    bug fix: missing rsync path; add chmod (may fail)

--- 40rsync.sh
+++ 40rsync.sh
@@ -1,16 +1,19 @@
-# Copy rsync to the data store drive 
+# Copy rsync to the data store drive
 
 STORE_DIR=/.internal
 CONFIG_DIR="$STORE_DIR"/donottouch
 
 if [ -z "$ERM_ROOT" ]; then
     echo Environment ERM_ROOT unknown
-elif [ ! -f rsync ]; then
-    echo no rsync binary to copy to $CONFIG_DIR
+elif [ ! -f $ERM_ROOT/rsync ]; then
+    echo no rsync binary in $ERM_ROOT to copy to $CONFIG_DIR
 elif [ -f $CONFIG_DIR/rsync ]; then
     echo rsync binary already exists in $CONFIG_DIR
 fi
 
 cp -p $ERM_ROOT/rsync $CONFIG_DIR/
+echo copy rsync returned $?
 
-echo copy returned $?
+# this may fail (e.g. on VFAT)
+chmod 777 $CONFIG_DIR/rsync
+#echo chmod rsync returned $?
