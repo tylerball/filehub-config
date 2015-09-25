65a4514df049beeb4ab55ac451d0b4f3230293dc
Steve Sixty-Four <steve8x8@googlemail.com>
2015-09-25 16:54:04 +0200

    Major rewrite of /etc/init.d/swap: - only to be applied if SWAP defined in /etc/firmware (skip WD03 .016, F800 .064) - use .vst/ instead of .internal/donottouch (like WD03 .014 FW) - ToDo: check whether swap is already in use; use lock?

--- 50swap.sh
+++ 50swap.sh
@@ -1,41 +1,74 @@
 # Add a swapfile on the data store drive 
 # (rsync needs this for large file copies)
 
+# Skip if /etc/init.d/swap is already in FW
+# 2015-09-25: use (existing) directory .vst/ for hosting swapfile
+
+if ! grep -q SWAP= /etc/firmware
+then
+{
+# F800 .064 and WD03 .016 firmwares appear to use/create .vst/swapfile
+echo "No SWAP= setting found in /etc/firmware, not modifying swap"
+}
+else
+{
+# enable SWAP
 sed -i 's/^SWAP=.*/SWAP=swap/' /etc/firmware
-
+# create swap init script, to overwrite the "exit 0" one
 cat <<'EOF' > /etc/init.d/swap
-STORE_DIR=/.internal
-CONFIG_DIR="$STORE_DIR"/donottouch
-rm -f /tmp/swapinfo
-
+# Swap initialization on external medium
+#rm -f /tmp/swapinfo
 echo "Running $0" > /tmp/swapinfo
 while read device mountpoint fstype remainder; do
-    echo "Checking $device at $mountpoint" >> /tmp/swapinfo
-    if [ ${device:0:7} == "/dev/sd" -a -e "$mountpoint$CONFIG_DIR" ];then
-            swapfile="$mountpoint$CONFIG_DIR"/swapfile
-            if [ ! -e "$swapfile" ]; then
-                echo "Creating swapfile $swapfile" >> /tmp/swapinfo
-                # large blocksize is flash-friendlier
-                dd if=/dev/zero of="$swapfile" bs=64k count=1024
-                # create swapfile signature
-                mkswap "$swapfile"
+    echo "Checking $device at $mountpoint"
+    if [ ${device:0:7} == "/dev/sd" -a -e "$mountpoint/.vst" ]
+    then
+        swapfile="$mountpoint/.vst/swapfile"
+        # exit if $swapfile already in use
+        #if grep "^$swapfile\b" /proc/swaps
+        #then
+        #    echo "Already using $swapfile"
+        #    exit 0
+        #fi
+        if [ -e "$swapfile" ]
+        then
+            echo "Found $swapfile"
+            ls -l "$swapfile"
+        else
+            echo "Creating $swapfile"
+            # large blocksize is flash-friendlier
+            dd if=/dev/zero of="$swapfile" bs=1024k count=64
+            # may have failed (lack of space etc.)
+            if [ $? -ne 0 ]
+            then
+                rm -f "$swapfile"
+                continue
             fi
-            swapon "$swapfile" >> /tmp/swapinfo 2>&1
-            if [ $? -eq 0 ]; then
-                echo "Turned on swap for $swapfile" >> /tmp/swapinfo
-            else
-                echo "There was an error turning on swap" >> /tmp/swapinfo
-            fi
-            exit 0
+            sync
+        fi
+        file "$swapfile"
+        # create swapfile signature
+        mkswap "$swapfile"
+        swapon "$swapfile"
+        if [ $? -ne 0 ]
+        then
+            echo "$swapfile not used for swap, removing"
+            rm -f "$swapfile"
+            continue
+        fi
+        echo "Swap activated on $swapfile ($fstype)"
+        exit 0
     fi
-done < /proc/mounts
+done < /proc/mounts >> /tmp/swapinfo 2>&1
 exit 0
 EOF
-
-# Make executable, and run right now!
+# Make executable, and run right now to create and test
 chmod +x /etc/init.d/swap
 echo pre-apply:
 free
 /etc/init.d/swap
 echo post-apply:
 free
+echo swap log:
+cat /tmp/swapinfo
+}
