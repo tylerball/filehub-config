2e90989aca1664c257d1f5c11fbaccc92394b21a
Steve Sixty-Four <steve8x8@googlemail.com>
2015-09-26 11:10:48 +0200

    NTP sync every 3 hours; add some explanation

--- 15ntp.sh
+++ 15ntp.sh
@@ -4,8 +4,11 @@ cat <<'EOF' > /etc/ntp/ntp.cfg
 # must be != 0 for NTP to be used
 switch=1
 # number of hours between syncs
-time=1
-# only first entry is used? better don't rely on DNS
+time=3
+# older firmwares used only first server entry
+# newer firmwares use max. four server entries
+# depending on firewall settings, we cannot always rely on DNS
+# this is for Germany and Central Europe:
 #server=ptbtime2.ptb.de:1.de.pool.ntp.org:0.europe.pool.ntp.org:2.asia.pool.ntp.org
 server=192.53.103.104:ptbtime2.ptb.de:1.de.pool.ntp.org:0.europe.pool.ntp.org
 EOF
