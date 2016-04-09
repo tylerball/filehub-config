7c5d9bf7281ccefc41088b19a16e30ec50f94527
Steve Sixty-Four <steve8x8@googlemail.com>
2016-04-09 13:21:35 +0200

    03savefirmware.sh fix

--- 03savefirmware.sh
+++ 03savefirmware.sh
@@ -1,7 +1,8 @@
 # save currently installed firmware
 # we don't know the current date but we can get the hw/fw versions
 . /etc/firmware
-SAVEDIR=`dirname $0`/$PRODUCTLINE-$CURVER
+SAVEDIR=`dirname $0`/fw-$PRODUCTLINE-$CURFILE-$CURVER
+mkdir -p $SAVEDIR
 for mtd in 0 1 2 3 4 5 6 7 8
 do
     echo mtdblock$mtd
