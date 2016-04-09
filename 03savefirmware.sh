d165a8baafb035ff2768378f29e5ce8828ae850d
Steve Sixty-Four <steve8x8@googlemail.com>
2016-04-09 13:06:28 +0200

    Untested (\!) addition to save the currently installed firmware to SDcard

---
+++ 03savefirmware.sh
@@ -0,0 +1,11 @@
+# save currently installed firmware
+# we don't know the current date but we can get the hw/fw versions
+. /etc/firmware
+SAVEDIR=`dirname $0`/$PRODUCTLINE-$CURVER
+for mtd in 0 1 2 3 4 5 6 7 8
+do
+    echo mtdblock$mtd
+    dd if=/dev/mtdblock$mtd of=$SAVEDIR/mtdblock$mtd bs=64k
+done
+ls -l $SAVEDIR/mtdblock*
+
